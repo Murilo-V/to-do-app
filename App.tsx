f709d55b97283155b6fa0b1e75134471b29e97e5
Murilo-V <alveiramurilo@hotmail.com>
2021-03-09 12:46:25 -0300

    feat: font raleway

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Routes from './src/routes';
+import { useFonts } from 'expo-font';
+import { Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold } from '@expo-google-fonts/raleway';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return null
+  }
+
   return (
     <Routes/>
   );
 }
+
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,8 +5,11 @@ import React from 'react';
 export default function Landing() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>todo</Text>
-            <StatusBar style="auto" />
+            <Text style={styles.text}>
+                tempo é <Text style={styles.investment}>investimento</Text>
+                .
+            </Text>
+            <StatusBar style="light" />
         </View>
     )
 }
@@ -14,13 +17,17 @@ export default function Landing() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#323232',
+      backgroundColor: '#00009E',
       alignItems: 'center',
       justifyContent: 'center',
     },
     text: {
-        fontWeight: 'bold',
-        fontSize: 40,
-        color: '#f2f2f2'
+        fontSize: 50,
+        color: '#FC0F90',
+        fontFamily: 'Raleway_700Bold',
+        textAlign: 'center',
+    },
+    investment: {
+        color: '#C319E6'
     }
   });
